725c0b4b312d4d3dfb04eb949351c0e8fb78b174
davekolian <davekolian@users.noreply.github.com>
2020-07-21 18:06:42 +0200

    Update webParse.py
    Quick update

--- Manga-RSS-App/webParse.py
+++ Manga-RSS-App/webParse.py
@@ -1,19 +1,29 @@
 from lxml import html
 import requests
+from urllib.request import Request, urlopen
 
 from kivy.app import App
-from kivy.app import Widget
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.graphics import Rectangle, Color
+from kivy.uix.widget import Widget
+from kivy.uix.anchorlayout import AnchorLayout
 
 from kivy.config import Config
-Config.set('graphics', 'width', 1000)
-Config.set('graphics', 'height', 1000)
 
+Config.set('graphics', 'width', 850)
+Config.set('graphics', 'height', 1000)
 
 not_read = []
 not_read_counter = 0
 no_of_manga = 0
+manga_imgs = []
+manga_imgs_counter = 0
+global g_var
 
 
 def func_find_daily_chaps():
@@ -33,6 +43,8 @@ def func_find_daily_chaps():
         chap = tree.xpath('//div[@class="row"]/span/a/text()')
         views = tree.xpath('//div[@class="row"]/span/text()')
         times = tree.xpath('//span/@title')
+        imgs_srcs = tree.xpath('//div[@class="manga-info-pic"]/img/@src')
+        # print(imgs_srcs)
 
         start = 0
         count = 1
@@ -63,6 +75,7 @@ def func_find_daily_chaps():
                     # print(manga, end="\n")
                     not_read.append("s")
                     not_read.append(manga[0])
+                    manga_imgs.append(imgs_srcs[0])
                     break
             elif "hour" in views[x] or "hours" in views[x]:
                 if int(str(views[x][0:2])) < 24:
@@ -70,6 +83,7 @@ def func_find_daily_chaps():
                     # print(manga, end="\n")
                     not_read.append("s")
                     not_read.append(manga[0])
+                    manga_imgs.append(imgs_srcs[0])
                     break
             elif "mins" in views[x] or "min" in views[x] or "minutes" in views[x] or "minute" in views[x]:
                 if int(str(views[x][0:1])) < 60:
@@ -77,6 +91,7 @@ def func_find_daily_chaps():
                     # print(manga, end="\n")
                     not_read.append("s")
                     not_read.append(manga[0])
+                    manga_imgs.append(imgs_srcs[0])
                     break
 
         for x in range(0, len(views)):
@@ -117,7 +132,6 @@ def func_find_daily_chaps():
                 'https://leviatanscans.com/comics/337225-the-rebirth-of-the-demon-god',
                 'https://zeroscans.com/comics/204586-omniscient-readers-point-of-view',
                 'https://zeroscans.com/comics/55416-record-of-the-war-god',
-                'https://zeroscans.com/comics/188504-second-life-ranker',
                 'https://zeroscans.com/comics/133460-yong-heng-zhi-zun',
                 'https://reaperscans.com/comics/915623-god-of-blackfield',
                 'https://reaperscans.com/comics/140270-the-great-mage-returns-after-4000-years']
@@ -128,8 +142,10 @@ def func_find_daily_chaps():
 
         manga = tree.xpath('//h5[@class="text-highlight"]/text()')
         manga_clean = str(manga).replace("\\n", "")
-        manga_clean = manga_clean.replace("['", "")
-        manga_clean = manga_clean.replace("']", "")
+        manga_clean = manga_clean.replace("'", "")
+        manga_clean = manga_clean.replace("[", "")
+        manga_clean = manga_clean.replace("]", "")
+        manga_clean = manga_clean.replace('"', "")
 
         chap = tree.xpath('//span[@class="text-muted text-sm"]/text()')
 
@@ -141,6 +157,8 @@ def func_find_daily_chaps():
         for x in range(0, len(dates)):
             dates[x] = str(dates[x]).replace("\n", " ")
 
+        imgs_srcs = tree.xpath('//a[@class="media-content"]/@style')
+
         for x in range(0, len(dates)):
             if "day" in dates[x] or "days" in dates[x]:
                 if int(str(dates[x][1:2])) < 2:
@@ -148,6 +166,16 @@ def func_find_daily_chaps():
                     # print(manga_clean, end="\n")
                     not_read.append("s")
                     not_read.append(manga_clean)
+                    if "leviatan" in url_list[url_counter]:
+                        imgs_srcs = str(imgs_srcs[0]).replace("background-image:url(", "https://leviatanscans.com")
+                        imgs_srcs = imgs_srcs.replace(")", "")
+                    elif "reaper" in url_list[url_counter]:
+                        imgs_srcs = str(imgs_srcs[0]).replace("background-image:url(", "https://reaperscans.com")
+                        imgs_srcs = imgs_srcs.replace(")", "")
+                    else:
+                        imgs_srcs = str(imgs_srcs[0]).replace("background-image:url(", "https://zeroscans.com")
+                        imgs_srcs = imgs_srcs.replace(")", "")
+                    manga_imgs.append(imgs_srcs)
                     break
             elif "hour" in dates[x] or "hours" in dates[x]:
                 if int(str(dates[x][1:2])) < 24:
@@ -155,6 +183,16 @@ def func_find_daily_chaps():
                     # print(manga_clean, end="\n")
                     not_read.append("s")
                     not_read.append(manga_clean)
+                    if "leviatan" in url_list[url_counter]:
+                        imgs_srcs = str(imgs_srcs[0]).replace("background-image:url(", "https://leviatanscans.com")
+                        imgs_srcs = imgs_srcs.replace(")", "")
+                    elif "reaper" in url_list[url_counter]:
+                        imgs_srcs = str(imgs_srcs[0]).replace("background-image:url(", "https://reaperscans.com")
+                        imgs_srcs = imgs_srcs.replace(")", "")
+                    else:
+                        imgs_srcs = str(imgs_srcs[0]).replace("background-image:url(", "https://zeroscans.com")
+                        imgs_srcs = imgs_srcs.replace(")", "")
+                    manga_imgs.append(imgs_srcs)
                     break
             elif "mins" in dates[x] or "min" in dates[x] or "minutes" in dates[x] or "minute" in dates[x]:
                 if int(str(dates[x][0:2])) < 60:
@@ -162,6 +200,16 @@ def func_find_daily_chaps():
                     # print(manga_clean, end="\n")
                     not_read.append("s")
                     not_read.append(manga_clean)
+                    if "leviatan" in url_list[url_counter]:
+                        imgs_srcs = str(imgs_srcs[0]).replace("background-image:url(", "https://leviatanscans.com")
+                        imgs_srcs = imgs_srcs.replace(")", "")
+                    elif "reaper" in url_list[url_counter]:
+                        imgs_srcs = str(imgs_srcs[0]).replace("background-image:url(", "https://reaperscans.com")
+                        imgs_srcs = imgs_srcs.replace(")", "")
+                    else:
+                        imgs_srcs = str(imgs_srcs[0]).replace("background-image:url(", "https://zeroscans.com")
+                        imgs_srcs = imgs_srcs.replace(")", "")
+                    manga_imgs.append(imgs_srcs)
                     break
 
         for x in range(0, len(dates)):
@@ -198,7 +246,7 @@ def func_find_daily_chaps():
                 'https://manganelo.com/manga/read_one_punch_man_manga_online_free3',
                 'https://manganelo.com/manga/black_clover', 'https://manganelo.com/manga/uaxz925974686',
                 'https://manganelo.com/manga/dnha19771568647794', 'https://manganelo.com/manga/doulou_dalu_manga',
-                'https://manganelo.com/manga/pn918005', 'https://manganelo.com/manga/ad921253']
+                'https://manganelo.com/manga/pn918005', 'https://manganelo.com/manga/ad921253','https://manganelo.com/manga/dz919342']
 
     while url_counter < len(url_list):
 
@@ -208,6 +256,7 @@ def func_find_daily_chaps():
         manga = tree.xpath('//div[@class="story-info-right"]/h1//text()')
         chap = tree.xpath('//a[@class="chapter-name text-nowrap"]/text()')
         dates = tree.xpath('//span[@class="chapter-time text-nowrap"]/text()')
+        imgs_srcs = tree.xpath('//span[@class="info-image"]/img/@src')
 
         for x in range(0, len(dates)):
             if "day" in dates[x] or "days" in dates[x]:
@@ -216,6 +265,7 @@ def func_find_daily_chaps():
                     # print(manga, end="\n")
                     not_read.append("s")
                     not_read.append(manga[0])
+                    manga_imgs.append(imgs_srcs[0])
                     break
             elif "hour" in dates[x] or "hours" in dates[x]:
                 if int(str(dates[x][0:2])) < 24:
@@ -223,11 +273,13 @@ def func_find_daily_chaps():
                     # print(manga, end="\n")
                     not_read.append("s")
                     not_read.append(manga[0])
+                    manga_imgs.append(imgs_srcs[0])
                     break
             elif "mins" in dates[x] or "min" in dates[x] or "minutes" in dates[x] or "minute" in dates[x]:
                 if int(str(dates[x][0:1])) < 60:
                     not_read.append("s")
                     not_read.append(manga[0])
+                    manga_imgs.append(imgs_srcs[0])
                     break
 
         for x in range(0, len(dates)):
@@ -281,6 +333,8 @@ def func_find_daily_chaps():
             dates[x] = str(dates[x]).replace("\n", "")
             dates[x] = str(dates[x]).replace("\t", "")
 
+        imgs_srcs = tree.xpath('//img[@class="img-responsive effect-fade ls-is-cached lazyloaded"]/@src')
+
         for x in range(0, len(dates)):
             if "day" in dates[x] or "days" in dates[x]:
                 if int(str(dates[x][0:1])) < 2:
@@ -288,6 +342,7 @@ def func_find_daily_chaps():
                     # print(manga, end="\n")
                     not_read.append("s")
                     not_read.append(manga[0])
+                    manga_imgs.append(imgs_srcs[0])
                     break
             elif "hour" in dates[x] or "hours" in dates[x]:
                 if int(str(dates[x][0:2])) < 24:
@@ -295,6 +350,7 @@ def func_find_daily_chaps():
                     # print(manga, end="\n")
                     not_read.append("s")
                     not_read.append(manga[0])
+                    manga_imgs.append(imgs_srcs[0])
                     break
             elif "mins" in dates[x] or "min" in dates[x] or "minutes" in dates[x] or "minute" in dates[x]:
                 if int(str(dates[x][0:2])) < 60:
@@ -302,6 +358,7 @@ def func_find_daily_chaps():
                     # print(manga, end="\n")
                     not_read.append("s")
                     not_read.append(manga[0])
+                    manga_imgs.append(imgs_srcs[0])
                     break
 
         for x in range(0, len(dates)):
@@ -330,6 +387,18 @@ def func_find_daily_chaps():
         url_counter += 1
 
 
+def func_find_imgs_manga_active():
+    for x in range(0, no_of_manga):
+        req = Request(manga_imgs[x], headers={'User-Agent': 'Mozilla/5.0'})
+        # print(manga_imgs[x])
+
+        # if no update pop up no update
+        name = str(x) + ".jpg"
+        f = open(name, "wb")
+        f.write(urlopen(req).read())
+        f.close()
+
+
 func_find_daily_chaps()
 
 for item in not_read:
@@ -339,12 +408,52 @@ for item in not_read:
     else:
         print(item, end="|")
 
+func_find_imgs_manga_active()
 
 
-class WebParseApp(App):
-    def build(self):
-        layout = GridLayout(cols=3)
-        name_chapter_time = ""
+#ScrollView > GridLayout(cols=3) > ____Layout > Button (takes up the whole page) >
+
+class BabyGrids2(GridLayout):
+    def __init__(self):
+        GridLayout.__init__(self, rows=3)
+
+        #self.add_widget(Button(text="hi"))
+
+        # self.add_widget(Label(text="hi", pos_hint={'x': 0, 'top': 1}))
+        self.add_widget(Button(size_hint=(1,1)))
+
+
+
+
+
+class BabyGrids(FloatLayout):
+    def __init__(self, title, name_pic):
+        FloatLayout.__init__(self)
+
+        #img = Image(source="1.jpg")
+        #self.add_widget(img)
+        #self.add_widget(BabyGrids2())
+        img = Image(source=name_pic, allow_stretch=True, keep_ratio=False, pos_hint={'x': 0, 'top': 1})
+        self.add_widget(img)
+        print(title)
+        self.add_widget(Button(text=title[0], pos_hint={'x': 0, 'top': 1}, size_hint=(1, 0.3), disabled=True))
+        self.add_widget(Button(pos_hint={'x': 0, 'top': 0.6}, size_hint=(1, 0.3)))
+        self.add_widget(Button(pos_hint={'x': 0, 'top': 0.3}, size_hint=(1, 0.3)))
+        #self.add_widget(BabyGrids2())
+        #self.add_widget(Button())
+
+
+
+
+class MainGrid(GridLayout):
+    def __init__(self):
+        GridLayout.__init__(self, cols=3, row_force_default="True", row_default_height=400, height=self.minimum_height,
+                            size_hint=(1, None))
+        self.bind(minimum_height=self.setter('height'))
+
+        counter = 0
+
+        name_chapter_time = []
         x = 0
 
         for y in range(0, no_of_manga):
@@ -352,17 +461,30 @@ class WebParseApp(App):
                 x += 1
 
             while not_read[x] != "s":
-                name_chapter_time += not_read[x]
-                name_chapter_time += "\n"
-                if x+1 < len(not_read):
+                name_chapter_time.append(not_read[x])
+                name_chapter_time.append("\n")
+                if x + 1 < len(not_read):
                     x += 1
                 else:
-                    return layout
+                    break
 
-            layout.add_widget(Button(text=name_chapter_time))
-            name_chapter_time = ""
+            # layout.add_widget(Button(text=name_chapter_time))
+            name2 = str(y) + ".jpg"
+            img = Image(source=name2, allow_stretch=True, keep_ratio=False)
+            #self.add_widget(img)
+            self.add_widget(BabyGrids(name_chapter_time, name2))
+            name_chapter_time = []
 
-        return layout
+
+class ScrollBarView(ScrollView):
+    def __init__(self):
+        ScrollView.__init__(self, do_scroll_x="False", do_scroll_y="True", size=self.size, scroll_type=['bars'])
+        self.add_widget(MainGrid())
+
+
+class WebParseApp(App):
+    def build(self):
+        return ScrollBarView()
 
 
 WebParseApp().run()
