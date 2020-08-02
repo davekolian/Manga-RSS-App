0f3ef69dbcca5c96131e22df83857efef918a62e
davekolian <davekolian@users.noreply.github.com>
2020-08-02 05:20:52 +0200

    Major cleaning!
    Major cleaning!

--- Manga-RSS-App/webParse.py
+++ Manga-RSS-App/webParse.py
@@ -33,14 +33,10 @@ from functools import partial
 import mysql.connector as mysql
 
 # Global Variables
-not_read = []
 no_of_manga = 0
-manga_imgs = []
 chapter_links = []
-loading = 1
-chapt = []  # list of all recent chapters
-chapters = []  # list of all links to chapters in same order as chapt
 url_counter = 0
+result = ""
 
 
 # Function which creates a batch file to open the manga chapter in my browser of choice
@@ -52,376 +48,8 @@ def func_create_batch_files(link):
 
     os.system("open_manga.bat")
 
-"""
-# Function which scrapes the websites to find which chapters have been newly released
-def func_find_daily_chaps():
-    global not_read
-    global manga_imgs
-    global chapter_links
-
-
-
-    # Mangakakalot
-    url_counter = 0
-    url_list = ['https://mangakakalot.com/read-lm7ib158504847850',
-                'https://mangakakalot.com/read-ox3yk158504833790',
-                'https://mangakakalot.com/read-zs6sp158504840280',
-                'https://mangakakalot.com/read-uz2ai158504852768',
-                'https://mangakakalot.com/manga/ow923334',
-                'https://mangakakalot.com/read-ep8pm158504835723',
-                'https://mangakakalot.com/manga/rj922755',
-                'https://mangakakalot.com/read-ro4rv158504853379']
-
-    while url_counter < len(url_list):
-        page = requests.get(url_list[url_counter])
-        tree = html.fromstring(page.content)
-
-        manga = tree.xpath('//ul[@class="manga-info-text"]/li/h1/text()')
-        chap = tree.xpath('//div[@class="row"]/span/a/text()')
-        views = tree.xpath('//div[@class="row"]/span/text()')
-        times = tree.xpath('//span/@title')
-        imgs_srcs = tree.xpath('//div[@class="manga-info-pic"]/img/@src')
-        links = tree.xpath('//div[@class="row"]/span/a/@href')
-
-        start = -1
-
-        if " " not in manga:
-            manga = str(manga) + " "
-
-        manga_clean = str(manga).replace("\\n", "")
-        manga_clean = manga_clean.replace("'", "")
-        manga_clean = manga_clean.replace("[", "")
-        manga_clean = manga_clean.replace("]", "")
-        manga_clean = manga_clean.replace('"', "")
-
-        chap_clean = []
-
-        for x in range(0, len(chap)):
-            start_chapter = chap[x].find("Chapter")
-            if ":" in chap[x]:
-                end_line = chap[x].find(":")
-                chap_clean.append(str(chap[x][start_chapter:end_line]).replace("Chapter ", ""))
-            else:
-                chap_clean.append(str(chap[x][start_chapter:]).replace("Chapter ", ""))
-
-        # Cleans and organizes the data to be in a cleaner form
-        if views[0] == '0':
-            start = 0
-        elif views[1] == '0':
-            start = 1
-
-        for x in range(start, len(views) // 2, 1):
-            views.pop(x)
-
-        if start != -1:
-            for x in range(start, len(views) // 2, 1):
-                views.pop(x)
-                chap.pop(x)
-        # End of the cleaning
-
-        for x in range(0, len(views)):
-            if "day" in views[x] or "days" in views[x]:
-                if int(str(views[x][0:1])) < 2:
-                    not_read.append("s")
-                    not_read.append(manga_clean)
-                    manga_imgs.append(imgs_srcs[0])
-                    break
-            elif "hour" in views[x] or "hours" in views[x]:
-                if int(str(views[x][0:2])) < 24:
-                    not_read.append("s")
-                    not_read.append(manga_clean)
-                    manga_imgs.append(imgs_srcs[0])
-                    break
-            elif "mins" in views[x] or "min" in views[x] or "minutes" in views[x] or "minute" in views[x]:
-                if int(str(views[x][0:1])) < 60:
-                    not_read.append("s")
-                    not_read.append(manga_clean)
-                    manga_imgs.append(imgs_srcs[0])
-                    break
-
-        for x in range(0, len(views)):
-            if "day" in views[x] or "days" in views[x]:
-                if int(str(views[x][0:1])) < 2:
-                    not_read.append(chap_clean[x])
-                    not_read.append(views[x])
-                    chapter_links.append(links[x])
-            elif "hour" in views[x] or "hours" in views[x]:
-                if int(str(views[x][0:2])) < 24:
-                    not_read.append(chap_clean[x])
-                    not_read.append(views[x])
-                    chapter_links.append(links[x])
-            elif "mins" in views[x] or "min" in views[x] or "minutes" in views[x] or "minute" in views[x]:
-                if int(str(views[x][0:1])) < 60:
-                    not_read.append(chap_clean[x])
-                    not_read.append(views[x])
-                    chapter_links.append(links[x])
-
-        url_counter += 1
-
-    # Leviatan|Zero Scans|Reaper Scans
-
-    url_counter = 0
-    url_list = ['https://leviatanscans.com/comics/i-am-the-sorcerer-king/',
-                'https://leviatanscans.com/comics/chronicles-of-heavenly-demon',
-                'https://leviatanscans.com/comics/157887-a-returners-magic-should-be-special/',
-                'https://leviatanscans.com/comics/866673-the-descent-of-the-demonic-master',
-                'https://leviatanscans.com/comics/209074-slave-b',
-                'https://leviatanscans.com/comics/656006-auto-hunting',
-                'https://leviatanscans.com/comics/246282-medical-return',
-                'https://leviatanscans.com/comics/11268-survival-story-of-a-sword-king-in-a-fantasy-world',
-                'https://leviatanscans.com/comics/337225-the-rebirth-of-the-demon-god',
-                'https://leviatanscans.com/comics/524614-rebirth-of-an-8-circled-mage/',
-                'https://zeroscans.com/comics/204586-omniscient-readers-point-of-view',
-                'https://zeroscans.com/comics/55416-record-of-the-war-god',
-                'https://zeroscans.com/comics/133460-yong-heng-zhi-zun',
-                'https://reaperscans.com/comics/915623-god-of-blackfield',
-                'https://reaperscans.com/comics/140270-the-great-mage-returns-after-4000-years']
-
-    while url_counter < len(url_list):
-        page = requests.get(url_list[url_counter])
-        tree = html.fromstring(page.content)
-
-        manga = tree.xpath('//h5[@class="text-highlight"]/text()')
-        manga_clean = str(manga).replace("\\n", "")
-        manga_clean = manga_clean.replace("'", "")
-        manga_clean = manga_clean.replace("[", "")
-        manga_clean = manga_clean.replace("]", "")
-        manga_clean = manga_clean.replace('"', "")
-
-        if " " not in manga_clean:
-            manga_clean = manga_clean + " "
-
-        chap = tree.xpath('//span[@class="text-muted text-sm"]/text()')
-
-        for x in range(0, len(chap)):
-            chap[x] = "Chapter " + str(chap[x]).replace("\n", "")
-
-        chap_clean = []
-
-        for x in range(0, len(chap)):
-            start_chapter = chap[x].find("Chapter")
-            if ":" in chap[x]:
-                end_line = chap[x].find(":")
-                chap_clean.append(str(chap[x][start_chapter:end_line]).replace("Chapter ", ""))
-            else:
-                chap_clean.append(str(chap[x][start_chapter:]).replace("Chapter ", ""))
-
-        for x in range(0, len(chap_clean)):
-            if " " in chap_clean[x]:
-                chap_clean[x] = chap_clean[x].replace(" ", "")
-
-        dates = tree.xpath('//a[@class="item-company text-muted h-1x"]/text()')
-
-        for x in range(0, len(dates)):
-            dates[x] = str(dates[x]).replace("\n", " ")
-
-        imgs_srcs = tree.xpath('//a[@class="media-content"]/@style')
-        links = tree.xpath('//a[@class="item-author text-color "]/@href')
-
-        for x in range(0, len(dates)):
-            if "day" in dates[x] or "days" in dates[x]:
-                if int(str(dates[x][1:2])) < 2:
-                    not_read.append("s")
-                    not_read.append(manga_clean)
-                    if "leviatan" in url_list[url_counter]:
-                        imgs_srcs = str(imgs_srcs[0]).replace("background-image:url(", "https://leviatanscans.com")
-                        imgs_srcs = imgs_srcs.replace(")", "")
-                    elif "reaper" in url_list[url_counter]:
-                        imgs_srcs = str(imgs_srcs[0]).replace("background-image:url(", "https://reaperscans.com")
-                        imgs_srcs = imgs_srcs.replace(")", "")
-                    else:
-                        imgs_srcs = str(imgs_srcs[0]).replace("background-image:url(", "https://zeroscans.com")
-                        imgs_srcs = imgs_srcs.replace(")", "")
-                    manga_imgs.append(imgs_srcs)
-                    break
-            elif "hour" in dates[x] or "hours" in dates[x]:
-                if int(str(dates[x][1:2])) < 24:
-                    not_read.append("s")
-                    not_read.append(manga_clean)
-                    if "leviatan" in url_list[url_counter]:
-                        imgs_srcs = str(imgs_srcs[0]).replace("background-image:url(", "https://leviatanscans.com")
-                        imgs_srcs = imgs_srcs.replace(")", "")
-                    elif "reaper" in url_list[url_counter]:
-                        imgs_srcs = str(imgs_srcs[0]).replace("background-image:url(", "https://reaperscans.com")
-                        imgs_srcs = imgs_srcs.replace(")", "")
-                    else:
-                        imgs_srcs = str(imgs_srcs[0]).replace("background-image:url(", "https://zeroscans.com")
-                        imgs_srcs = imgs_srcs.replace(")", "")
-                    manga_imgs.append(imgs_srcs)
-                    break
-            elif "mins" in dates[x] or "min" in dates[x] or "minutes" in dates[x] or "minute" in dates[x]:
-                if int(str(dates[x][0:2])) < 60:
-                    not_read.append("s")
-                    not_read.append(manga_clean)
-                    if "leviatan" in url_list[url_counter]:
-                        imgs_srcs = str(imgs_srcs[0]).replace("background-image:url(", "https://leviatanscans.com")
-                        imgs_srcs = imgs_srcs.replace(")", "")
-                    elif "reaper" in url_list[url_counter]:
-                        imgs_srcs = str(imgs_srcs[0]).replace("background-image:url(", "https://reaperscans.com")
-                        imgs_srcs = imgs_srcs.replace(")", "")
-                    else:
-                        imgs_srcs = str(imgs_srcs[0]).replace("background-image:url(", "https://zeroscans.com")
-                        imgs_srcs = imgs_srcs.replace(")", "")
-                    manga_imgs.append(imgs_srcs)
-                    break
-
-        for x in range(0, len(dates)):
-            if "day" in dates[x] or "days" in dates[x]:
-                if int(str(dates[x][1:2])) < 2:
-                    not_read.append(chap_clean[x])
-                    not_read.append(dates[x])
-                    chapter_links.append(links[x])
-            elif "hour" in dates[x] or "hours" in dates[x]:
-                if int(str(dates[x][1:2])) < 24:
-                    not_read.append(chap_clean[x])
-                    not_read.append(dates[x])
-                    chapter_links.append(links[x])
-            elif "mins" in dates[x] or "min" in dates[x] or "minutes" in dates[x] or "minute" in dates[x]:
-                if int(str(dates[x][0:2])) < 60:
-                    not_read.append(chap_clean[x])
-                    not_read.append(dates[x])
-                    chapter_links.append(links[x])
-
-        url_counter += 1
-
-    # Manganelo
-    url_counter = 0
-    url_list = ['https://manganelo.com/manga/xn921310', 'https://manganelo.com/manga/apotheosis',
-                'https://manganelo.com/manga/huku267071576897767', 'https://manganelo.com/manga/hyer5231574354229',
-                'https://manganelo.com/manga/read_boku_no_hero_academia_manga',
-                'https://manganelo.com/manga/read_one_punch_man_manga_online_free3',
-                'https://manganelo.com/manga/black_clover', 'https://manganelo.com/manga/uaxz925974686',
-                'https://manganelo.com/manga/dnha19771568647794', 'https://manganelo.com/manga/doulou_dalu_manga',
-                'https://manganelo.com/manga/pn918005', 'https://manganelo.com/manga/ad921253',
-                'https://manganelo.com/manga/dz919342', 'https://manganelo.com/manga/wu_dong_qian_kun']
-
-    while url_counter < len(url_list):
-
-        page = requests.get(url_list[url_counter])
-        tree = html.fromstring(page.content)
-
-        manga = tree.xpath('//div[@class="story-info-right"]/h1//text()')
-        chap = tree.xpath('//a[@class="chapter-name text-nowrap"]/text()')
-        dates = tree.xpath('//span[@class="chapter-time text-nowrap"]/text()')
-        imgs_srcs = tree.xpath('//span[@class="info-image"]/img/@src')
-        links = tree.xpath('//a[@class="chapter-name text-nowrap"]/@href')
-
-        chap_clean = []
-
-        if " " not in manga:
-            manga = str(manga) + " "
-
-        manga_clean = str(manga).replace("\\n", "")
-        manga_clean = manga_clean.replace("'", "")
-        manga_clean = manga_clean.replace("[", "")
-        manga_clean = manga_clean.replace("]", "")
-        manga_clean = manga_clean.replace('"', "")
-
-        for x in range(0, len(chap)):
-            if "Chapter" in chap[x]:
-                start_chapter = chap[x].find("Chapter")
-
-                if ":" in chap[x]:
-                    end_line = chap[x].find(":")
-                    chap_no = str(chap[x][start_chapter:end_line]).replace("Chapter", "")
-                    if " " in chap_no:
-                        chap_clean.append(chap_no.replace(" ", ""))
-                    else:
-                        chap_clean.append("SC")
-                elif " -" in chap[x]:
-                    end_line = chap[x].find(" -")
-                    chap_no = str(chap[x][start_chapter:end_line]).replace("Chapter", "")
-                    if " " in chap_no:
-                        chap_clean.append(chap_no.replace(" ", ""))
-                    else:
-                        chap_clean.append("SC")
-                else:
-                    chap_no = str(chap[x][start_chapter:]).replace("Chapter", "")
-                    if " " in chap_no:
-                        chap_clean.append(chap_no.replace(" ", ""))
-                    else:
-                        chap_clean.append("SC")
-            else:
-                chap_clean.append("SC")
-
-        for x in range(0, len(dates)):
-            if "day" in dates[x] or "days" in dates[x]:
-                if int(str(dates[x][0:1])) < 2:
-                    not_read.append("s")
-                    not_read.append(manga_clean)
-                    manga_imgs.append(imgs_srcs[0])
-                    break
-            elif "hour" in dates[x] or "hours" in dates[x]:
-                if int(str(dates[x][0:2])) < 24:
-                    not_read.append("s")
-                    not_read.append(manga_clean)
-                    manga_imgs.append(imgs_srcs[0])
-                    break
-            elif "mins" in dates[x] or "min" in dates[x] or "minutes" in dates[x] or "minute" in dates[x]:
-                if int(str(dates[x][0:1])) < 60:
-                    not_read.append("s")
-                    not_read.append(manga_clean)
-                    manga_imgs.append(imgs_srcs[0])
-                    break
-
-        for x in range(0, len(dates)):
-            if "day" in dates[x] or "days" in dates[x]:
-                if int(str(dates[x][0:1])) < 2:
-                    not_read.append(chap_clean[x])
-                    not_read.append(dates[x])
-                    chapter_links.append(links[x])
-            elif "hour" in dates[x] or "hours" in dates[x]:
-                if int(str(dates[x][0:2])) < 24:
-                    not_read.append(chap_clean[x])
-                    not_read.append(dates[x])
-                    chapter_links.append(links[x])
-            elif "mins" in dates[x] or "min" in dates[x] or "minutes" in dates[x] or "minute" in dates[x]:
-                if int(str(dates[x][0:2])) < 60:
-                    not_read.append(chap_clean[x])
-                    not_read.append(dates[x])
-                    chapter_links.append(links[x])
-
-        url_counter += 1
-
 
 # Function to download cover image of all manga chapters released | method used to override that a spider is crawling
-def func_find_imgs_manga_active():
-    for x in range(0, no_of_manga):
-        req = Request(manga_imgs[x], headers={'User-Agent': 'Mozilla/5.0'})
-
-        name = str(x) + ".jpg"
-        f = open(name, "wb")
-        f.write(urlopen(req).read())
-        f.close()
-
-
-# Function which gets all details, images, image names, etc.
-def func_get_stuff():
-    global chapters
-    global chapt
-
-    func_find_daily_chaps()
-
-    for item in not_read:
-        if str(item) == "s":
-            print("\n")
-            global no_of_manga
-            no_of_manga += 1
-
-    func_find_imgs_manga_active()
-
-    # The "s" is added to show a split between each manga
-    for item in not_read:
-        if item != "s":
-            chapters.append(item)
-
-    # To get all the new chapters into an array
-    for x in range(0, len(chapters)):
-        if " " not in chapters[x]:
-            global chapt
-            chapt.append(chapters[x])
-"""
-
 def func_find_imgs_manga_active(img, x):
     req = Request(img, headers={'User-Agent': 'Mozilla/5.0'})
 
@@ -430,9 +58,8 @@ def func_find_imgs_manga_active(img, x):
     f.write(urlopen(req).read())
     f.close()
 
-result = ""
 
-
+# Function to connect to my database and read the tuples in the table
 def connect_to_database():
     connection = mysql.connect(host="sql7.freemysqlhosting.net", database="sql7358070", user="sql7358070",
                                password="2iyy8UBTtE")
@@ -462,9 +89,8 @@ def connect_to_database():
 class BabyGrids(FloatLayout):
     def __init__(self, chapter_name, the_chapters, img_name):
         super(BabyGrids, self).__init__()
-        global chapt
-        global chapter_links
         global url_counter
+        cha1 = []
 
         img = Image(source=img_name, allow_stretch=True, keep_ratio=False, pos_hint={'x': 0, 'top': 1})
         self.add_widget(img)
@@ -484,11 +110,6 @@ class BabyGrids(FloatLayout):
         self.add_widget(Button(text=ch_name, pos_hint={'x': 0, 'top': 1}, size_hint=(1, 0.25), font_size='16sp',
                                background_color=(0, 0, 0, 0.3)))
 
-        cha1 = []
-        cha2 = []
-
-        # cha1 will contain values without 's' and '\n' in the format of [name, ch_no, time, ch_no, time, ...]
-        ch_spl = ""
         if "," in the_chapters:
             les_words2 = str(the_chapters).split(",")
             for item in les_words2:
