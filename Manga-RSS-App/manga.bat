463ef62924cc5d3f33a50119c980e61303184ba6
davekolian <davekolian@users.noreply.github.com>
2020-09-15 23:38:36 +0200

    Small update on .bat

--- Manga-RSS-App/manga.bat
+++ Manga-RSS-App/manga.bat
@@ -1,2 +1,2 @@
-D:\Documents\GitHub\Manga-RSS-App\virt_env\Scripts\python.exe D:\Documents\GitHub\Manga-RSS-App\Manga-RSS-App\webParse.py
+D:\Documents\GitHub\Manga-RSS-App\virt_env\Scripts\python.exe webParse.py
 exit
