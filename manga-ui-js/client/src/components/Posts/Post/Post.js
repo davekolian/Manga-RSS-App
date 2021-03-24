065ae8e9be0546bc42901c73921e2edd6179836f
davekolian <50661033+davekolian@users.noreply.github.com>
2021-03-24 14:07:13 +0400

    Added some more CSS customization

--- manga-ui-js/client/src/components/Posts/Post/Post.js
+++ manga-ui-js/client/src/components/Posts/Post/Post.js
@@ -4,16 +4,30 @@ import styles from "./styles.css";
 const Post = ({ post }) => {
   const items = [];
 
+  const mainDivStyle = {
+    position: "relative",
+    margin: "10px",
+    width: "225px",
+    height: "337px",
+    backgroundImage: `url(${post.img_link_bg})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    display: "inline-block",
+  };
+
   for (var i = 0; i < post.manga_chapters.length; i++) {
-    items.push(<a href={post.chapter_links[i]}>{post.manga_chapters[i]}</a>);
+    items.push(
+      <a style={styles.a} href={post.chapter_links[i]}>
+        {post.manga_chapters[i]}
+      </a>
+    );
   }
 
   return (
     <>
-      <div>
-        <h1>{post.manga_name}</h1>
-        <img src={post.img_link_bg} alt="{Manga} Background" />
-        {items}
+      <div class="main" style={mainDivStyle}>
+        <p style={styles.p}>{post.manga_name}</p>
+        <div class="chaps">{items}</div>
       </div>
     </>
   );
