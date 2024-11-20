cfa241cfcf6c522beee146e09f7c9562b0a54653
davekolian <50661033+davekolian@users.noreply.github.com>
2024-11-20 19:01:20 +0400

    Added the new API link. Fixed an issue with thunk. Updated the public folder.

--- manga-ui-js/client/src/index.js
+++ manga-ui-js/client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
+import {thunk} from "redux-thunk";
 
 import reducers from "./reducers";
 
