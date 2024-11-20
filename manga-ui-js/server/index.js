2d003c4517d78be21757b9f041e046039f8dcf79
davekolian <50661033+davekolian@users.noreply.github.com>
2024-11-20 17:07:20 +0400

    Added a new CORS method since the client couldn't access it

--- manga-ui-js/server/index.js
+++ manga-ui-js/server/index.js
@@ -1,21 +1,33 @@
 import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-
 import postRoutes from './routes/posts.js';
 
 const app = express();
+app.use(function (req, res, next) {
+	const allowedOrigins = [
+		'http://localhost:5000',
+		'http://localhost:3000',
+		'https://manga-rss-app.netlify.app/',
+	];
+	const origin = req.headers.origin;
+	if (allowedOrigins.includes(origin)) {
+		res.setHeader('Access-Control-Allow-Origin', origin);
+	}
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	);
+	res.header('Access-Control-Allow-credentials', true);
+	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, UPDATE');
+	next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 
 app.use('/posts', postRoutes);
 app.get('/', (req, res) => {
 	res.send('Hello to Manga-RSS-App Api!');
 });
-
-dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
