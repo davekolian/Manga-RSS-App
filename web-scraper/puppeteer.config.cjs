63b317bc1512caa30de233aa87d1994c6f4ddd4c
davekolian <50661033+davekolian@users.noreply.github.com>
2024-11-21 16:29:41 +0400

    Uploading back to Render

---
+++ web-scraper/puppeteer.config.cjs
@@ -0,0 +1,13 @@
+const { join } = require('path');
+
+/**
+ * @type {import("puppeteer").Configuration}
+ */
+module.exports = {
+	// Changes the cache location for Puppeteer.
+	cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+	launch: {
+		headless: true,
+	},
+	browserContext: 'default',
+};
---
+++ web-scraper/render-build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# exit on errorset -o errexit
+
+npm install
+# npm run build # uncomment if required
+
+# Store/pull Puppeteer cache with build cache
+if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then 
+  echo "...Copying Puppeteer Cache from Build Cache" 
+  cp -R $XDG_CACHE_HOME/puppeteer/ $PUPPETEER_CACHE_DIR
+else 
+  echo "...Storing Puppeteer Cache in Build Cache" 
+  cp -R $PUPPETEER_CACHE_DIR $XDG_CACHE_HOME
+fi
